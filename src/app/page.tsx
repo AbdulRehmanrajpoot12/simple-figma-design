c3cc4a4397cb6f05e0b5c9e0d963904f6d9be745
Abdul Rehman Rajpoot <165961815+AbdulRehmanrajpoot12@users.noreply.github.com>
2024-11-05 21:14:33 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import HeaderPage from "@/components/HeaderSection/page";
 import HeroPage from "@/components/HeroSection/page";
-import Image from "next/image";
 
 export default function Home() {
   return (
